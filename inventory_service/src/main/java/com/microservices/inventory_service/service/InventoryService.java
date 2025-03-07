3cf7049aafd67c562c965d8521078236cdb0be18
kamimore <negivipin996@gmail.com>
2025-03-07 16:17:47 +0530

    making the changes to the inventory

---
+++ inventory_service/src/main/java/com/microservices/inventory_service/service/InventoryService.java
@@ -0,0 +1,28 @@
+package com.microservices.inventory_service.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.microservices.inventory_service.dto.InventoryResponse;
+import com.microservices.inventory_service.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode)
+        .stream().map(inventory -> 
+             InventoryResponse.builder()
+            .skuCode(inventory.getSkuCode())
+            .isInStock(inventory.getQuantity() > 0)
+            .build()
+        ).toList();
+    }
+}
